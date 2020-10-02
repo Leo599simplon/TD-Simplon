76a158ae90a9d4067828a7f90125ad9479de2c2e
Simplon <josepjoestar224@gmail.com>
2020-10-02 15:20:55 +0400

    wut

---
+++ TaskList/ToDoFunction.js
@@ -0,0 +1,8 @@
+function Popup() {
+    var popwindow = document.getElementById("checkBundle");
+    if (popwindow.style.display === "none") {
+        popwindow.style.display = "block";
+   } else {
+         popwindow.style.display = "none";
+     }
+};
