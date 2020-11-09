399a70783371a1c6a441d49b4af4b5052a5491e4
Sutra <caddy@Fraicheur>
2020-11-09 21:15:57 +0400

    exo requete POST

--- Exos/Exo envoi requete formulaire/javascript.js
+++ Exos/Exo envoi requete formulaire/javascript.js
@@ -1,13 +1,12 @@
-var value = document.getElementById('value')
-var result = document.getElementById('result')
-var form = document.getElementById('form')
+const value = document.getElementById('value')
+const form = document.getElementById('form')
+const result = document.getElementById('result')
 
 let request = new XMLHttpRequest();
 
-
-form.addEventListener('submit',function(e) {
-
-e.preventDefault();
+form.addEventListener('submit', (e) => {
+    
+    e.preventDefault();
     request.onreadystatechange = function() {
     
     if (this.readyState == 4 && this.status == 200) {
@@ -19,15 +18,6 @@ e.preventDefault();
 request.open("POST", "https://mockbin.com/request");
 request.setRequestHeader("Content-Type", "application/json");
 request.send(JSON.stringify(value.value));
-  console.log(value.value);
-  
+console.log(value.value);
 
-  
 });
-
-
-
-
-
-                                    
-  
